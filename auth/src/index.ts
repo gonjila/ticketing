d3acebba25416d50acd973f4720d5f843c37493a
gonjila <lgonjila@gmail.com>
2024-12-13 06:20:20 +0400

    config: skaffold and kubernetes services

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const PORT = 8000;
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json("aaaaa23");
+});
+
 app.listen(PORT, () => {
   console.log(`Running on http://localhost:${PORT}`);
 });
